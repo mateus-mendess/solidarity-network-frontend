5973b7fb6adc22cb63b40edf07efc6f96e9db7ed
mateus-mendess <mendes.profissional12@gmail.com>
2025-10-13 15:04:42 -0300

    feat: implementing the authenticationService and configuring axios to store the JWT token in the header

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
+import { login } from "@/service/authenticationService";
 import AccountTypeSelectorCard from "./AccountTypeSelectorCard";
 import Modal from "../components/Modal";
 
 function LoginCard() {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [open, setOpen] = useState(false);
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = await login(email, password);
+      console.log("Login bem-sucedido: ", data)
+    } catch {
+      alert("Falha no login!")
+    }
+  }
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="border-2 shadow-[10px_10px_0px_rgba(156,163,175,1)] rounded-2xl p-8 w-full max-w-md">
         <h2 className="text-6xl text-center font-bold text-blue-400">Login</h2>
 
-        <form method="Post" className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label htmlFor="email" className="block font-medium text-gray-700">
               Email
@@ -22,6 +33,8 @@ function LoginCard() {
             <input
               type="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Digite seu email"
               className="mt-1 w-full px-4 py-2 border-2 rounded-full border-gray-700 focus:outline-none"
             />
---
+++ src/service/authenticationService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import api from "./api";
+
+export async function login(email: string, password: string) {
+    try{
+        const response = await api.post("/auth/login", {
+            email,
+            password
+        });
+
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+
+        return response.data;
+    } catch(error: unknown) {
+        if (axios.isAxiosError(error)) {
+            console.error("Erro de login: ", error.response?.data || error.message);
+        }
+        throw error;
+    }
+}
